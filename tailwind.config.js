92d8b739ee4fb3fe3c8f340e607a9dd50d3c2b71
Niall Colman <niallcolman@Nialls-MacBook-Pro-2.local>
2023-01-27 16:31:13 +0000

    *Added Favorit font* *Added front page layout and responsive styling* *Images added* *text marquee css added* *Package lock file*

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'i4brands-slate': '#868882',
+        'i4brands-grey': '#d5d5cd',
+        'i4brands-off-white': '#f8f8f2',
+      },
+      fontFamily: {
+        favorit: "Favorit, sans-serif",
+      },
+      animation: {
+        marquee: 'marquee 25s linear infinite',
+        marquee2: 'marquee2 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
