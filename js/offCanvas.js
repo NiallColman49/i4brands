d1ee8fcff4fa5ff9bbc4408aa30ed84d0e025a5b
Niall Colman <niallcolman@Nialls-MacBook-Pro.local>
2023-02-09 11:11:08 +0000

    *functionality added to hamburger menu and applied on all pages*

--- js/offCanvas.js
+++ js/offCanvas.js
@@ -1,6 +1,13 @@
 const hamburger = document.querySelector(".hamburger");
-console.log(hamburger);
+
 hamburger.addEventListener("click", () => {
+    const menu = document.querySelector(".off-canvas-menu");
+    menu.classList.toggle("hidden");
+});
+
+const closeMenu = document.querySelector(".close");
+
+closeMenu.addEventListener("click", () => {
     const menu = document.querySelector(".off-canvas-menu");
     menu.classList.toggle("hidden");
 });
